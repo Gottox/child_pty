8d8c5bba9679fb18b291871a05ddbe4b4b22290f
Enno Boland <g@s01.de>
2014-11-14 11:45:38 +0100

    fix mixed quotations.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-var assert = require("assert");
-var child_pty = require("../index");
+var assert = require('assert');
+var child_pty = require('../index');
 
 describe('child_pty.spawn()', function(){
 	var child;
 	function spawn_client() {
 		child = child_pty.spawn(process.argv[0],
-			Array.prototype.concat.apply([ __dirname + "/libexec/client.js"],
+			Array.prototype.concat.apply([ __dirname + '/libexec/client.js'],
 			   arguments));
 	}
 
@@ -18,14 +18,14 @@ describe('child_pty.spawn()', function(){
 	});
 	it('should print to stdout', function(done){
 		spawn_client('print');
-		var d = "";
+		var d = '';
 
 		child.stdout.on('data', function(data) {
 			d += data.toString();
 		});
 
 		child.on('exit', function() {
-			assert("print\r\n", d, "should receive data");
+			assert('print\r\n', d, 'should receive data');
 			done();
 		});
 	});
@@ -39,20 +39,20 @@ describe('child_pty.spawn()', function(){
 		});
 
 		child.on('exit', function() {
-			assert("data\r\n", d, 'received data should be ok');
+			assert('data\r\n', d, 'received data should be ok');
 			done();
 		});
 
-		child.stdin.write("data\n");
+		child.stdin.write('data\n');
 	});
 	it('should fire event on resize', function(done) {
 		spawn_client();
 		var resized = false;
 
-		child.on('resize', function(data) {
+		child.on('resize', function() {
 			resized = true;
 		}).on('exit', function() {
-			assert.ok(resized, "resize should be emitted");
+			assert.ok(resized, 'resize should be emitted');
 			done();
 		});
 
@@ -63,13 +63,13 @@ describe('child_pty.spawn()', function(){
 
 		child.stdout.once('data', function(data) {
 			if(data.toString() !== 'print\r\n') {
-				assert.ok(false, "received wrong data");
+				assert.ok(false, 'received wrong data');
 				child.kill();
 				return done();
 			}
 			child.resize({ columns:80, rows: 80 });
 			child.stdout.once('data', function(data) {
-				assert("sigwinch\r\n", data);
+				assert('sigwinch\r\n', data);
 				child.kill();
 			});
 		});
