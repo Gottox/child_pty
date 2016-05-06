d00527982bb367ddf2587674a7a029655066cc10
Enno Boland <g@s01.de>
2016-05-06 11:27:20 +0200

    src/exechelper: fix segfault / potentional buffer overruns on exotic platforms.

--- src/exechelper.cpp
+++ src/exechelper.cpp
@@ -8,8 +8,12 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-	int rv, state_fd = atoi(argv[1]);
+	int state_fd;
+	uint32_t rv;
 
+	if(argc < 3)
+		return EXIT_FAILURE;
+	state_fd = atoi(argv[1]);
 	if (fcntl(state_fd, F_SETFD, fcntl(state_fd, F_GETFD) | FD_CLOEXEC) >= 0 &&
 			ioctl(STDIN_FILENO, TIOCSCTTY, NULL) >= 0) {
 		argc -= 2;
@@ -18,7 +22,7 @@ int main(int argc, char *argv[]) {
 		execvp(argv[0], argv);
 	}
 	rv = htonl(errno);
-	write(state_fd, &rv, sizeof(errno));
+	write(state_fd, &rv, sizeof(rv));
 	pause();
 	return EXIT_FAILURE;
 }
