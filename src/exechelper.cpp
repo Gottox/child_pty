463f063e8a559c9e0f4e2d8596c64addcec8cafe
Enno Boland <g@s01.de>
2017-01-18 23:33:16 +0100

    do not call TIOCSCTTY on STDIN but on the real first PTY.

--- src/exechelper.cpp
+++ src/exechelper.cpp
@@ -8,18 +8,19 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-	int master_fd, state_fd;
+	int master_fd, slave_fd, state_fd;
 	uint32_t rv;
 
-	if(argc < 4)
+	if(argc < 5)
 		return EXIT_FAILURE;
 	state_fd = atoi(argv[1]);
 	master_fd = atoi(argv[2]);
+	slave_fd = atoi(argv[3]);
 	if (fcntl(state_fd, F_SETFD, fcntl(state_fd, F_GETFD) | FD_CLOEXEC) >= 0 &&
-			ioctl(STDIN_FILENO, TIOCSCTTY, NULL) >= 0 &&
+			(slave_fd < 0 || ioctl(slave_fd, TIOCSCTTY, NULL) >= 0) &&
 			close(master_fd) >= 0) {
-		argc -= 3;
-		memmove(argv, argv + 3, argc * sizeof(char*));
+		argc -= 4;
+		memmove(argv, argv + 4, argc * sizeof(char*));
 		argv[argc] = NULL;
 		execvp(argv[0], argv);
 	}
